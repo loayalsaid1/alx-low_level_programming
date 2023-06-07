9e073ca54e0e643cbe8d345f12c90041a6dc28d8
loayalsaid1 <l.programming555@gmail.com>
2023-06-07 16:48:59 -0700

    Add 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+/*
+ * _puts_recursion - provides a similar function to  puts
+ * s: the string
+ * return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+		return;
+	_putchar(s[0]);
+	_puts_recursion(s + 1);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+0-puts_recursion.c: Alternitave to puts function
+0-puts_recursion.c: Alternitave to puts function
+
