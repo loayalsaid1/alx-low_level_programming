738956c80524de7faa4405af9ccb838c5440b13f
loayalsaid1 <l.programming555@gmail.com>
2023-06-09 16:14:43 -0700

    Add 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+#include "main.h"
+/**
+ * main - main fuction
+ * @argc: count
+ * @argv: argumets
+ * Return: 0 on success and 1 on error
+ */
+int main(int argc,char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
