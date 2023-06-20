8501ed67d53f1722548ee2d5c64cea5b01c4fb95
loayalsaid1 <l.programming555@gmail.com>
2023-06-19 19:19:14 -0700

    create a struch dog in dog.h

---
+++ 0x0D-preprocessor/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - struct for dog attributes
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner of dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
