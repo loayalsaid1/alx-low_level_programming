1a62047542550fec0b07c7630da668d75ea21461
Loay Al-Said <l.programming555@gmail.com>
2023-06-24 00:12:05 +0300

    .d

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,23 +11,23 @@
  */
 int main(int argc, char *argv[])
 {
-    int n1, n2;
+        int n1, n2;
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+        if (argc != 4)
+        {
+            printf("Error\n");
+            exit(98);
+        }
 
-    n1 = atoi(argv[1]);
-    n2 = atoi(argv[3]);
+        n1 = atoi(argv[1]);
+        n2 = atoi(argv[3]);
 
-    if (get_op_func(argv[2]) == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+        if (get_op_func(argv[2]) == NULL)
+        {
+            printf("Error\n");
+            exit(99);
+        }
 
-    printf("%i\n", get_op_func(argv[2])(n1, n2));
-    return (0);
+        printf("%i\n", get_op_func(argv[2])(n1, n2));
+        return (0);
 }
