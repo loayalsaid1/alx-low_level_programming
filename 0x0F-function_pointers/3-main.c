2000fe2166430c80f3a7267cb3e905334a913c35
Loay Al-Said <l.programming555@gmail.com>
2023-06-23 21:24:03 +0200

    program that perfroms simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+
+/**
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ *
+int main(int argc, char *argv[])
+{
+    int n1;
+    int n2;
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+    n1 = atoi(argv[1]);
+    n2 = atoi(argv[3]);
+
+    if (get_op_func(argv[2]) == NULL)
+    {
+        printf("Error\n");
+        exit(99);
+    }
+    printf("%i\n", (get_op_func(argv[2])(n1, n2)));
+    return (0);
+}
