1db3e109e679799fc4809514df8745c3a906e427
Loay Al-Said <l.programming555@gmail.com>
2023-07-01 05:34:46 +0300

    teh same

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,16 +8,16 @@
  */
 void free_list(list_t *head)
 {
-        list_t *temp;
+	list_t *temp;
 
-        while (head != NULL)
-        {
-                temp = head;
-                head = head->next;
-                free(temp->str);
-                free(temp);
-        }
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
 
-        head = NULL;
+	head = NULL;
 }
 
