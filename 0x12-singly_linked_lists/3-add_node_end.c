bc3352ea995530cf49b575ee5082c3d90a13cd0c
Loay Al-Said <l.programming555@gmail.com>
2023-07-01 05:18:43 +0300

    Fix betty style for add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,38 +2,39 @@
 
 /**
  * add_node_end - Add a node to the end of the list
- * 
+ *
  * @head: a pointer to the pointer of the first node
  * @str: the string to fill the last node with
- * 
+ *
  * Return: pointer to the node of NULL on fail
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
-        list_t *current, *new_node;
+	
+	list_t *current, *new_node;
 
-        if (str == NULL)
-                return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-        new_node = malloc(sizeof(list_t));
-        if (new_node == NULL)
-                return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-        new_node->str = strdup(str);
-        new_node->len = strlen(str);
-        new_node->next = NULL;
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
 
-        if (*head == NULL)
-                *head = new_node;
-        else
-        {
-                current = *head;
-                while (current->next != NULL)
-                {
-                        current = current->next;
-                }
-                current->next = new_node;
-        }
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node;
+	}
 
-        return (new_node);
+	return (new_node);
 }
