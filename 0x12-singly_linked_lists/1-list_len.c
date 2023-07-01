9e80cb32e8e8b56d78b563bec5948c9f207e43b2
Loay Al-Said <l.programming555@gmail.com>
2023-07-01 03:16:21 +0300

    change indetation to tabs

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,13 +10,13 @@
  */
 size_t list_len(const list_t *h)
 {
-        size_t nodes;
+	size_t nodes;
 
-        nodes = 0;
-        while (h != NULL)
-        {
-                nodes++;
-                h = h->next;
-        }
-        return (nodes);
+	nodes = 0;
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
 }
