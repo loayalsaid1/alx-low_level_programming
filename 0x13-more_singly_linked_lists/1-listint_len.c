8524200737ed1c64eb749d598bed7efd0d318abe
Loay Al-Said <l.programming555@gmail.com>
2023-07-04 19:00:29 +0200

    try

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,26 +1,23 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a list
+ * listint_len - returns the number of elements
+ * in a linked list
  *
  * @h: pointer to head node
  *
- * Description: Increase the count and point to the next one and repeat unliss
- * it's NULL. all as an iteration prossec
- * Return: the number of nodes
+ * Return: number of elements
+ *
  */
 size_t listint_len(const listint_t *h)
 {
 	size_t count;
 
 	count = 0;
-	while (h != NULL)
+	while (h)
 	{
-		count++;
-
 		h = h->next;
+		count++;
 	}
-
 	return (count);
 }
-
