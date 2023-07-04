a0eefd6c4f58b20a89b622e37ea7a662af470c73
Loay Al-Said <l.programming555@gmail.com>
2023-07-05 01:20:41 +0200

    Decrease the condition by one in the for loop

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,7 +19,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	current = head;
 
-	for (i = 0; i <= index; i++)
+	for (i = 0; i < index; i++)
 	{
 		if (current == NULL)
 			return (NULL);
