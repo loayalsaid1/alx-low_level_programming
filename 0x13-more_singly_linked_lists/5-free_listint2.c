215dbbce5e090fdda467fa1509201ad743abde49
Loay Al-Said <l.programming555@gmail.com>
2023-07-05 00:10:49 +0200

    Add 5-free_listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Free a linked lists that starts with the
+ * node pointed to by @*head
+ *
+ * @*head: A pointer to the pointer points to the first node
+ *
+ * Desciption: Free the node and go to the next and do the same
+ * untill it's the end (next == NULL)
+ *
+ * Return: It doesn't
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	if (*head == NULL)
+	{
+		head = NULL;
+		return;
+	}
+
+	current = *head;
+	while ((*head)->next != NULL)
+	{
+		*head = current->next;
+
+		free(current);
+
+		current = *head;
+	}
+	free(*head);
+
+	*head = NULL;
+	head = NULL;
+	current = NULL;
+}
