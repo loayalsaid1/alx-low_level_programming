615ba3c516162fbeeb76c4005f56cee637d5e6df
Loay Al-Said <l.programming555@gmail.com>
2023-07-05 02:31:05 +0200

    Fix an edge case(idx = 0)

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,6 +26,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 
 	current = *head;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 	for (i = 0; i < idx; i++)
 	{
 		if (current == NULL)
