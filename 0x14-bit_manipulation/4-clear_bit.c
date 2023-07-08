2ef274151740512b2973611e133d509ed883b2d7
Loay Al-Said <l.programming555@gmail.com>
2023-07-08 02:13:27 +0200

    Try to change the variable n

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,13 +10,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (n == NULL)
-		return (-1);
-
 	if (index > 63)
 		return (-1);
 
-	*n &= ~(1 << index);
+	*n = *n & ~(1 << index);
 
 	return (1);
 }
