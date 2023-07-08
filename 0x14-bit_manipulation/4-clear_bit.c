b485ed6bd5c36f05af72bb6f298255a67434ccbf
Loay Al-Said <l.programming555@gmail.com>
2023-07-08 02:16:19 +0200

    Fix it, it was just the prototype

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,10 +10,13 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	if (n == NULL)
+		return (-1);
+
 	if (index > 63)
 		return (-1);
 
-	*n = *n & ~(1 << index);
+	*n &= ~(1 << index);
 
 	return (1);
 }
