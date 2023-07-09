b54a62a623b077de83c1a6d349273451228ebd5e
Loay Al-Said <l.programming555@gmail.com>
2023-07-09 08:29:11 +0200

    Make 9-insert_nodeint.c work with head == NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,19 +17,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *current, *new_node, *temp;
 	unsigned int i;
 
-	if (head == NULL || *head == NULL)
-		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	current = *head;
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
+	current = *head;
 	for (i = 0; i < idx; i++)
 	{
 		if (current == NULL)
