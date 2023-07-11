2098a78a2e4ade1c778630fe38980b2c55b13be2
Loay Al-Said <92930196+loayalsaid1@users.noreply.github.com>
2023-07-11 06:40:11 +0200

    Make small fixes to

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,8 +29,8 @@ int _strlen(char *str)
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int lenght;
-	int writen_bytes;
+	int writen_bytes = 0;
+	int lenght = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -38,7 +38,9 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	lenght = _strlen(text_content);
+	
+	if (text_content != NULL)
+		lenght = _strlen(text_content);
 	if ((write(fd, text_content, lenght)) == -1)
 		return (-1);
 
