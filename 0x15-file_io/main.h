bfcd3c364617f39cef2f01b88ce6d81853027450
Loay Al-Said <l.programming555@gmail.com>
2023-07-11 03:50:48 +0200

    Fix betty issues in main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,8 +4,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-/*#include <sys/stat.h>
-#include <sys/types.h>*/
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
