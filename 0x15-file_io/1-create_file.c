92e35b15c48788c7e4349dd86c5aed81b8f69cca
Loay Al-Said <92930196+loayalsaid1@users.noreply.github.com>
2023-07-11 04:52:09 +0200

    Fix betty issue

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,7 +37,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	
+
 	if (text_content != NULL)
 		lenght = _strlen(text_content);
 	if ((write(fd, text_content, lenght)) == -1)
