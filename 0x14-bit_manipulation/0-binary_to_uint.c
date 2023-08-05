a4e567f9d17e7b102c117ec964f85e35c5749306
Loay Al-Said <l.programming555@gmail.com>
2023-08-04 17:28:06 -0700

    Add 0-binary_to_int.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,44 +1,59 @@
 #include "main.h"
 
 /**
- * binary_to_uint - Returns and decimal represintation of a binary number given
- * in a string
+ * _strlen - returns the length of a string
+ * @str: string to be processed
  *
- * @b: the string
+ * ********** IMPORTANT NOTE: I THOUGH IT MAY
+ * BE MORE BETTER IF I USE A COUNTER VARIABLE
+ * BUT IT TURNED OUT THAT NO!
  *
- * Return: 0 on errors occurence and the number on success
+ *
+ * one more edit: i will change the i to make it
+ * named counter, to have a middle ground
+ * and make it more readable
+ *
+ * Return: lenth of string
  */
+int _strlen(const char *str)
+{
+	int counter;
+
+	for (counter = 0; str[counter] != '\0'; counter++)
+		;
+	return (counter);
+}
+
+/**
+ * binary_to_uint - convert binary into uint
+ *
+ * @b: the string contains the string
+ *
+ * Return: 0 in fail, or the final uint
+*/
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
-	int i;
+	unsigned int result;
+	int i, len;
 
-	if (validate_string(b))
+	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i] != '\0'; i++)
+	len = _strlen(b);
+
+	for (i = 0; i < len; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+
+	result = 0;
+	for (i = 0; i < len; i++)
 	{
 		result <<= 1;
-
 		if (b[i] == '1')
 			result += 1;
 	}
 
 	return (result);
-}
-
-int validate_string(const char *given)
-{
-	int i;
-
-	if (given == NULL)
-		return (1);
-
-	for (i = 0; given[i] != '\0'; i++)
-	{
-		if (given[i] != '0' && given[i] != '1')
-			return (1);
-	}
-
-	return (0);
 }
