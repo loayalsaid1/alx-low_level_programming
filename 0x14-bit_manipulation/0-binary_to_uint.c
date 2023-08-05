3d842c0487d7ae69f733b3dbca9b48200661f0a3
Loay Al-Said <l.programming555@gmail.com>
2023-08-04 17:15:25 -0700

    Add 0-binary_to_int.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+int validate_string(const char *given);
+
+/**
+ * binary_to_uint - Returns and decimal represintation of a binary number given
+ * in a string
+ *
+ * @b: the string
+ *
+ * Return: 0 on errors occurence and the number on success
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int i;
+
+	if (validate_string(b))
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		result <<= 1;
+
+		if (b[i] == '1')
+			result += 1;
+	}
+
+	return (result);
+}
+
+int validate_string(const char *given)
+{
+	int i;
+
+	if (given == NULL)
+		return (1);
+
+	for (i = 0; given[i] != '\0'; i++)
+	{
+		if (given[i] != '0' && given[i] != '1')
+			return (1);
+	}
+
+	return (0);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int validate_string(const char *given);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int);
