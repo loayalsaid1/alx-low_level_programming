0caa2334b7e6ac573434420263315d01b532c17d
Loay Al-Said <l.programmig555@gmail.com>
2023-12-06 10:16:05 +0200

    Fix HT task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,10 +11,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (table == NULL)
 		return (NULL);
+
 	table->size = size;
-	table->array = malloc(size * sizeof(hash_node_t));
+	table->array = calloc(size, sizeof(hash_node_t *));
 	if (table->array == NULL)
+	{
+		free(table);
 		return (NULL);
+	}
 
 	return (table);
 }
