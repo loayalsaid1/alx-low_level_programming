4d0dcebbc0eeeb6dc5367a3029f7617facbe50f9
Loay Al-Said <l.programming555@gmail.com>
2024-05-22 15:28:11 +0300

    add documentation for interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,6 +1,13 @@
 #include "search_algos.h"
 
 
+/**
+ * interpolation_search - Interplation search for sorted int array
+ * @array: The array to search
+ * @size: the size fo the array
+ * @value: Target value
+ * Return: Index of <value>; -1 if not possible`
+*/
 int interpolation_search(int *array, size_t size, int value)
 {
 	size_t pos, low_index, high_index, low_value, high_value;
