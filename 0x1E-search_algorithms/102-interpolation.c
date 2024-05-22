72479a909a95e9c51986a9e41994c57f89719808
Loay Al-Said <l.programming555@gmail.com>
2024-05-22 15:23:26 +0300

    102-interpolation.c

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,45 @@
+#include "search_algos.h"
+
+
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t pos, low_index, high_index, low_value, high_value;
+
+	if (array == NULL || size < 1)
+		return (-1);
+
+	low_index = 0;
+	high_index = size - 1;
+
+	while (low_index < high_index)
+	{
+		low_value = array[low_index];
+		high_value = array[high_index];
+
+		pos = low_index + (((double)(high_index - low_index) / (
+			high_value - low_value)) * (value - low_value));
+
+		if (pos > high_index)
+		{
+			printf("Value checked array[%ld] is out of range\n", pos);
+			return (-1);
+		}
+
+		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+
+		if (value == array[pos])
+			return (pos);
+		else if (value > array[pos])
+			low_index = pos + 1;
+		else
+			high_index = pos - 1;
+	}
+
+	if (value == array[low_index])
+	{
+		printf("Value checked array[%ld] = [%d]\n", pos, array[low_index]);
+
+		return (low_index);
+	}
+	return (-1);
+}
