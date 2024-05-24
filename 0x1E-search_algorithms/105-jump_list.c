b8f98587183703c7968dea11b5ea86aae015cb7d
Loay Al-Said <l.programming555@gmail.com>
2024-05-24 21:03:56 +0300

    105-jump_list.c

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,29 +1,45 @@
 #include "search_algos.h"
 #include <math.h>
 
+
+/**
+ * jump_list - Simulate jump search algorithm for linked lists
+ * @list: Pointer to hte head of the list
+ * @size: siz of hte list
+ * @value: the target value
+ * Return: pointer to the node with the target value or Null if not possible`
+*/
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	listint_t *tmp;
-	size_t jump = sqrt(size);
-	size_t i;
-	size_t left = 0;
+	int i, jump = sqrt(size);
+	listint_t *temp = list;
+	listint_t *prev = list;
+
 	if (list == NULL || size < 1)
 		return (NULL);
-	
-	tmp = list;
 
-	while (tmp && tmp->n < value && i < size;)
+	while (value > temp->n && temp->next)
 	{
-		for (i = left; i < left + jump && tmp->next; i++)
-			tmp = tmp->next;
-		printf("jump %ld\n", tmp->index);
-		if tmp->index > value;
-
-		left = tmp->index;   
+		prev = temp;
+		/*Do the jump*/
+		for (i = 0; i < jump && temp->next; i++)
+		{
+			temp = temp->next;
+		}
+		printf("Value checked at index [%li] = [%i]\n", temp->index, temp->n);
 	}
 
-	if (tmp && tmp->n == value)
-		return tmp;
-	
+	printf("Value found between indexes [%li] and [%li]\n",
+		prev->index, temp->index);
+
+	temp = prev;
+	for (i = 0; i < jump && temp != NULL; i++)
+	{
+		printf("Value checked at index [%li] = [%i]\n", temp->index, temp->n);
+		if (temp->n == value)
+			return (temp);
+		temp = temp->next;
+	}
+
 	return (NULL);
 }
--- 0x1E-search_algorithms/main.c
+++ 0x1E-search_algorithms/main.c
@@ -1,6 +1,90 @@
+#include <stdlib.h>
+#include "search_algos.h"
+
+void free_list(listint_t *list);
+
+/**
+ * create_list - Creates a single linked list
+ *
+ * @array: Pointer to the array to use to fill the list
+ * @size: Size of the array
+ *
+ * Return: A pointer to the head of the created list (NULL on failure)
+ */
+listint_t *create_list(int *array, size_t size)
+{
+	listint_t *list;
+	listint_t *node;
+
+	list = NULL;
+	while (array && size--)
+	{
+		node = malloc(sizeof(*node));
+		if (!node)
+		{
+			free_list(list);
+			return (NULL);
+		}
+		node->n = array[size];
+		node->index = size;
+		node->next = list;
+		list = node;
+	}
+	return (list);
+}
+
+
+
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * free_list - Deallocates a singly linked list
+ *
+ * @list: Pointer to the linked list to be freed
+ */
+void free_list(listint_t *list)
+{
+	listint_t *node;
+
+	if (list)
+	{
+		node = list->next;
+		free(list);
+		free_list(node);
+	}
+}
+
+
+
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * print_list - Prints the content of a listint_t
+ *
+ * @list: Pointer to the head of the list
+ */
+void print_list(const listint_t *list)
+{
+	printf("List :\n");
+	while (list)
+	{
+		printf("Index[%lu] = [%d]\n", list->index, list->n);
+		list = list->next;
+	}
+	printf("\n");
+}
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "search_algos.h"
+
+listint_t *create_list(int *array, size_t size);
+void print_list(const listint_t *list);
+void free_list(listint_t *list);
 
 /**
  * main - Entry point
@@ -9,11 +93,22 @@
  */
 int main(void)
 {
+    listint_t *list, *res;
     int array[] = {
-    3, 4, 5, 7, 7, 7, 8, 9, 10, 11, 21, 31, 33, 33, 33, 44, 51
+        0, 1, 2, 3, 4, 7, 12, 15, 18, 19, 23, 53, 61, 62, 76, 99
     };
     size_t size = sizeof(array) / sizeof(array[0]);
 
-    printf("Found %d at index: %d\n\n", 31, advanced_binary(array, size, 8));
+    list = create_list(array, size);
+    print_list(list);
+
+    res =  jump_list(list, size, 53);
+    printf("Found %d at index: %lu\n\n", 53, res->index);
+    res =  jump_list(list, size, 2);
+    printf("Found %d at index: %lu\n\n", 2, res->index);
+    res =  jump_list(list, size, 999);
+    printf("Found %d at index: %p\n", 999, (void *) res);
+
+    free_list(list);
     return (EXIT_SUCCESS);
 }
